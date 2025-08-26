287703b9aa715e48f4e9528790e75b6e6c9a7b80
Luna Dellazzeri <luna.dellazzeri@etu.univ-lyon1.fr>
2025-08-26 14:39:08 +0200

    Added cosmology.py module.

---
+++ cosmology.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+"""
+This file contains a very simple function to calculate the Hubble Parameter (H(z)
+) as a function of redshift in a matter-dominated universe.
+"""
+def hubble(redshift):
+    hubble_const = 0.7
+    matter = 0.3 * (1 + redshift) ** 3
+    curvature = 0.0 * (1 + redshift) ** 2
+    dark_energy = 0.7
+
+    return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
