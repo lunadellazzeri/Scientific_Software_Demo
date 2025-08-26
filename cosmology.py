1a04430402bb04dc9933bda22fb8c9ca61d6b1ab
Luna Dellazzeri <luna.dellazzeri@etu.univ-lyon1.fr>
2025-08-26 14:44:08 +0200

    Removed hard-coded values.

--- cosmology.py
+++ cosmology.py
@@ -4,10 +4,21 @@ import numpy as np
 This file contains a very simple function to calculate the Hubble Parameter (H(z)
 ) as a function of redshift in a matter-dominated universe.
 """
+
+#hard-coded cosmological parameters version: 
 def hubble(redshift):
     hubble_const = 0.7
     matter = 0.3 * (1 + redshift) ** 3
     curvature = 0.0 * (1 + redshift) ** 2
     dark_energy = 0.7
+
+    return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
+
+#dictionary object as an argument version:
+def hubble(redshift, cosmo_dict):
+    hubble_const = cosmo_dict["H0"]
+    matter = cosmo_dict["omega_m_0"] * (1 + redshift) ** 3
+    curvature = cosmo_dict["omega_k_0"] * (1 + redshift) ** 2
+    dark_energy = cosmo_dict["omega_lambda_0"]
 
     return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
