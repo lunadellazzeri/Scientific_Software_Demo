01d8538f530211d5481c13f56af6d4b9dafbe1c4
Luna Dellazzeri <luna.dellazzeri@etu.univ-lyon1.fr>
2025-09-08 19:47:05 +0200

    Added tests directory, cosmology test unit and critical_density test unit

--- mycosmo/cosmology.py
+++ mycosmo/cosmology.py
@@ -8,6 +8,7 @@ Hubble Parameter (H(z)) as a function of redshift in a matter-dominated universe
 """
 
 #Hard-coded cosmological parameters
+"""
 def hubble(redshift):
     hubble_const = 0.7
     matter = 0.3 * (1 + redshift) ** 3
@@ -15,9 +16,10 @@ def hubble(redshift):
     dark_energy = 0.7
 
     return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
+"""
 
 #Dictionary object as an argument 
-#better : addd the function to a class and make these constants class attributes
+#better : add the function to a class and make these constants class attributes
 def hubble(redshift, cosmo_dict):
     hubble_const = cosmo_dict["H0"]
     matter = cosmo_dict["omega_m_0"] * (1 + redshift) ** 3
---
+++ mycosmo/tests/test_cosmology.py
@@ -0,0 +1,43 @@
+import numpy as np
+import numpy.testing as npt
+
+from mycosmo.cosmology import hubble, critical_density
+
+
+class TestCosmology:
+    fid_cosmo = {
+        "H0": 70,
+        "omega_m_0": 0.3,
+        "omega_k_0": 0.0,
+        "omega_lambda_0": 0.7,
+    }
+    H_tolerance = 0.01
+    rho_tolerance = 1e-27
+    z_range = np.array([0.0, 0.5, 1.0])
+    H_expect = np.array([70, 91.60, 123.24])
+
+    def test_hubble(self):
+        H_vals = hubble(self.z_range, self.fid_cosmo)
+
+        npt.assert_allclose(
+            H_vals,
+            self.H_expect,
+            atol=self.H_tolerance,
+            err_msg=(
+                "The H(z) differs from expected values by more than "
+                f"{self.H_tolerance} decimal places."
+            ),
+        )
+    
+    def test_critical_density(self):
+        rho_vals = critical_density(self.z_range, self.fid_cosmo)
+
+        npt.assert_allclose(
+            rho_vals,
+            self.rho_expect,
+            atol=self.rho_tolerance,
+            err_msg=(
+                "The rho(z) differs from expected values by more than "
+                f"{self.rho_tolerance} decimal places."
+            ),
+        )
