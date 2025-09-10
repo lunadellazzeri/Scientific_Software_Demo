c1f3abc09461586414c8c3073c7635d971bcab41
Luna Dellazzeri <luna.dellazzeri@etu.univ-lyon1.fr>
2025-09-10 10:41:38 +0200

    Implemented CI job

--- mycosmo/constants.py
+++ mycosmo/constants.py
@@ -5,5 +5,6 @@ This module contains all the cosmological constants needed.
 * ``G``: Gravitationnal constant, set to 6.6743e-11 kg⁻¹ s.
 * ``Mpc``: Megaparsec convertion, set to 3.08568e22 km.
 """
+
 G = 6.6743e-11
 Mpc = 3.08568e22
